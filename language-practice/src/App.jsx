a2a0e58a4f86fd0d0e544660bfc1ef592bfed7e3
Jkig <DerekLeroux47@gmail.com>
2023-08-12 15:04:51 -0600

    some more stuff, mainly text underlining and buttons

--- language-practice/src/App.jsx
+++ language-practice/src/App.jsx
@@ -6,8 +6,11 @@ function App() {
   const [textNative, setTextNative] = useState(["The customer is very important, the customer will be followed by the customer. It's a very important part of the game, it's done by advertising or, in a different way.", "In the weekend, there is no noise, but there is a lot of grief. There is a lake, but the course should be soft, the policy element before.", "He needs a home unless the cookbook is made up. Even if the country wants to be flattered by the borders, that's great. Each of the ullamcorper ultracis. Duis congue dolor lake, let it be a lot of dignissim fear of the members.", "Yes, and it takes a lot. Diseases of life members are important. It's soft now that anyone is made of trucks. Duis is not a pure gate; It was not easy for the family. But always unless. For the pillow of the earth. But that was neither.", "Feugiat's vestibulum, however, is flattering and flattering."])
   const [textTarget, setTextTarget] = useState(["Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sit amet diam fringilla, efficitur augue vel, varius sapien.", "In volutpat nulla et tortor tincidunt, at luctus lorem luctus. Vestibulum lacus est, cursus sed mollis sit amet, lobortis elementum ante.", "Duis eget nisi vitae tellus efficitur rutrum. Donec vel tellus egestas velit blandit finibus at id magna. Quisque ullamcorper ultrices lacus. Duis congue dolor lacus, sit amet dignissim metus sodales sed.", "Etiam et suscipit magna. Morbi vitae sodales magna. Praesent mollis nunc quis ex aliquet efficitur. Duis non purus porta, iaculis ante ornare, faucibus magna. Duis non facilisis lectus. Aliquam sed semper nisi. Nam ac pulvinar tellus. Sed id erat neque.", "Vestibulum feugiat sed ligula ac blandit."])
   const [speaker, setSpeaker] = useState(["You", "AI", "You", "AI", "You"])
+  const [spanish, setSpanish] = useState(true)
+
   
   const indecies = [0,1,2,3,4]// have this match size of those things
+  const incorrect = [false, false, false, true, false]
 
 
   return (
@@ -21,7 +24,11 @@ function App() {
               </ul>
         </li>
 
-        {indecies.map(index => <Response textNative={textNative[index]} textTarget={textTarget[index]} speaker={speaker[index]}/>)}
+        {indecies.map(index => <Response key={index} textNative={textNative[index]} textTarget={textTarget[index]} speaker={speaker[index]} incorrect={incorrect[index]}/>)}
+        <ul>
+          <li className="horizontal"><button onClick={() => setSpanish(!spanish)} className='langButton'>Speaking in {spanish ? "Spanish" : "English"}</button></li>
+          <li className="horizontal"><button onClick={() => console.log("stoptalking")} className='stopButton'>stopSpeaking</button></li>
+        </ul>
       </ul>
     </div>
   )
--- language-practice/src/response.jsx
+++ language-practice/src/response.jsx
@@ -2,9 +2,9 @@ export default function Response(props){
     return(
         <li>
             <ul>
-                <li className="horizontal">{props.speaker}: {props.textNative}</li>
+                <li className="horizontal">{props.speaker}: <div className={props.incorrect ? "incorrect" : "correct"}>{props.textNative}</div></li>
                 <li className="spacer5"></li>
-                <li className="horizontal">{props.speaker}: {props.textTarget}</li>
+                <li className="horizontal">{props.speaker}: <div className={props.incorrect ? "incorrect" : "correct"}>{props.textTarget}</div></li>
                 <p></p>
             </ul>
         </li>
